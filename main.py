9a1dca107437a23e6a34346a886e77c1b50efcf5
ymonni <512yns@gmail.com>
2023-05-09 23:02:43 -0500

    code

--- main.py
+++ main.py
@@ -1,206 +1,65 @@
-#Ymonni Simms 2106646
-import csv
 from datetime import datetime
+import csv
 
-class OutputInventory:
-
-    def __init__(self, item_list):
-        self.item_list = item_list
-
-    def full(self):
-
-        with open('./output_files/FullInventory.csv', 'w') as file:
-            items = self.item_list
-# get order of keys to write to file based on manufacturer
-            keys = sorted(items.keys(), key = items[x]['manufacturer'])
-            for item in keys:
-                id = item
-                man_name = items[item]['manufacturer']
-                item_type = items[item]['item_type']
-                price = items[item]['price']
-                service_date = items[item]['service_date']
-                damaged = items[item]['damaged']
-                file.write('{},{},{},{},{},{}\n'.format(id,man_name,item_type,price,service_date,damaged))
-
-    def by_type(self):
-
-        items = self.item_list
-        types = []
-        keys = sorted(items.keys())
-        for item in items:
-            item_type = items[item]['item_type']
-            if item_type not in types:
-                types.append(item_type)
-        for type in types:
-            file_name = type.capitalize() + 'Inventory.csv'
-            with open('./output_files/'+file_name, 'w') as file:
-                for item in keys:
-                    id = item
-                    man_name = items[item]['manufacturer']
-                    price = items[item]['price']
-                    service_date = items[item]['service_date']
-                    damaged = items[item]['damaged']
-                    item_type = items[item]['item_type']
-                    if type == item_type:
-                        file.write('{},{},{},{},{}\n'.format(id, man_name, price, service_date, damaged))
-
-    def past_service(self):
-
-        items = self.item_list
-        keys = sorted(items.keys(), key= datetime.strptime(items[x]['service_date'], "%m/%d/%Y").date(), reverse=True)
-        with open('./output_files/PastServiceDateInventory.csv', 'w') as file:
-            for item in keys:
-                id = item
-                man_name = items[item]['manufacturer']
-                item_type = items[item]['item_type']
-                price = items[item]['price']
-                service_date = items[item]['service_date']
-                damaged = items[item]['damaged']
-                today = datetime.now().date()
-                service_expiration = datetime.strptime(service_date, "%m/%d/%Y").date()
-                expired = service_expiration < today
-                if expired:
-                    file.write('{},{},{},{},{},{}\n'.format(id, man_name, item_type, price, service_date, damaged))
+class Item:
+    def init(self, itemID, itemType,manufacturername,damage,price):
+        self.manufacturername = manufacturername
+        self.itemID = itemID
+        self.ItemType=itemType
+        self.price=price
+        self.damage=damage
 
 
-    def damaged(self):
+def getprice(itemid):
+    index=0
+    max=-1
+    preader = csv.reader(open('PriceList.csv', 'r'))
+    preader=list(preader)
+    for prow in preader:
+        if itemid in prow[0]:
+            if max < int(prow[1]):
+                max= int(prow[1])
+            index=index+1
 
-        items = self.item_list
-# get order of keys to write to file based on price
-        keys = sorted(items.keys(), key= items[x]['price'], reverse=True)
-        with open('./output_files/DamagedInventory.csv', 'w') as file:
-            for item in keys:
-                id = item
-                man_name = items[item]['manufacturer']
-                item_type = items[item]['item_type']
-                price = items[item]['price']
-                service_date = items[item]['service_date']
-                damaged = items[item]['damaged']
-                if damaged:
-                    file.write('{},{},{},{},{}\n'.format(id, man_name, item_type, price, service_date))
+    return max
 
 
-if __name__ == '__main__':
-    items = {}
-    files = ['ManufacturerList.csv', 'PriceList.csv', 'ServiceDatesList.csv']
-    for file in files:
-        with open(file, 'r') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            for line in csv_reader:
-                item_id = line[0]
-                if file == files[0]:
-                    items[item_id] = {}
-                    man_name = line[1]
-                    item_type = line[2]
-                    damaged = line[3]
-                    items[item_id]['manufacturer'] = man_name.strip()
-                    items[item_id]['item_type'] = item_type.strip()
-                    items[item_id]['damaged'] = damaged
-                elif file == files[1]:
-                    price = line[1]
-                    items[item_id]['price'] = price
-                elif file == files[2]:
-                    service_date = line[1]
-                    items[item_id]['service_date'] = service_date
 
-    inventory = OutputInventory(items)
-# Create all the output files
-    inventory.full()
-    inventory.by_type()
-    inventory.past_service()
-    inventory.damaged()
+def checkdate(itemid):
+    check =0
+    sreader = csv.reader(open('ServiceDatesList.csv', 'r'))
+    sreader=list(sreader)
+    for srow in sreader:
+        if itemid in srow[0]:
+            if srow[1] < datetime.datetime.today().strftime('%m/%d/%Y'):
+                check=1
+
+    return check
 
 
-    types = []
-    manufacturers = []
-    for item in items:
-        checked_manufacturer = items[item]['manufacturer']
-        checked_type = items[item]['item_type']
-        if checked_manufacturer not in types:
-            manufacturers.append(checked_manufacturer)
-        if checked_type not in types:
-            types.append(checked_type)
+
+while 1:
+    m_name=input("Enter Manufacturer name:")
+    item_type=input("Enter Item Type")
+
+    reader = csv.reader(open('ManufacturerList.csv', 'r'))
+    reader=list(reader)
+    flag=0
+    for row in reader:
+        if m_name in row[1] and item_type in row[2]:
+            flag=1
+            if row[3] not in "damaged" and checkdate(row[0])==0:
+                print("Item Id=%s \n Manufacturer Name=%s \n Item Type= %s \n Price=%d" % row[0],row[1],row[2],getprice(row[0]))
+        if item_type in row[2]:
+            flag=1
+            print("\n Other Manufacturers: \n")
+            if row[3] not in "damaged" and checkdate(row[0])==0:
+                print("Item Id=%d \n Manufacturer Name=%s \n Item Type= %s \n Price=%d" % row[0],row[1],row[2],getprice(row[0]))
 
 
-    user_input = None
-    while user_input != 'q':
-        user_input = input("\nPlease enter an item manufacturer and item type (ex: Apple laptop) or enter 'q' to quit:\n")
-        if user_input == 'q':
-            break
-        else:
+    if flag==0:
+        print("No such item in inventory")
 
-            selected_manufacturer = None
-            selected_type = None
-            user_input = user_input.split()
-            bad_input = False
-            for word in user_input:
-                if word in manufacturers:
-                    if selected_manufacturer:
-
-                        bad_input = True
-                    else:
-                        selected_manufacturer = word
-                elif word in types:
-                    if selected_type:
-
-                        bad_input = True
-                    else:
-                        selected_type = word
-            if not selected_manufacturer or not selected_type or bad_input:
-                print("No such item in inventory")
-            else:
-
-                keys = sorted(items.keys(), key = items[x]['price'], reverse=True)
-
-
-                matching_items = []
-                similar_items = {}
-                for item in keys:
-                    if items[item]['item_type'] == selected_type:
-
-                        today = datetime.now().date()
-                        service_date = items[item]['service_date']
-                        service_expiration = datetime.strptime(service_date, "%m/%d/%Y").date()
-                        expired = service_expiration < today
-                        if items[item]['manufacturer'] == selected_manufacturer:
-                            if not expired and not items[item]['damaged']:
-                                matching_items.append((item, items[item]))
-                        else:
-                            if not expired and not items[item]['damaged']:
-                                similar_items[item] = items[item]
-
-
-                if matching_items:
-                    item = matching_items[0]
-                    item_id = item[0]
-                    man_name = item[1]['manufacturer']
-                    item_type = item[1]['item_type']
-                    price = item[1]['price']
-                    print("Your item is: {}, {}, {}, {}\n".format(item_id, man_name, item_type, price))
-
-
-                    if similar_items:
-                        matched_price = price
-                        closest_item = None
-                        closest_price_diff = None
-                        for item in similar_items:
-                            if closest_price_diff == None:
-                                closest_item = similar_items[item]
-                                closest_price_diff = abs(int(matched_price) - int(similar_items[item]['price']))
-                                item_id = item
-                                man_name = similar_items[item]['manufacturer']
-                                item_type = similar_items[item]['item_type']
-                                price = similar_items[item]['price']
-                                continue
-                            price_diff = abs(int(matched_price) - int(similar_items[item]['price']))
-                            if price_diff < closest_price_diff:
-                                closest_item = item
-                                closest_price_diff = price_diff
-                                item_id = item
-                                man_name = similar_items[item]['manufacturer']
-                                item_type = similar_items[item]['item_type']
-                                price = similar_items[item]['price']
-                    print("You may, also, consider: {}, {}, {}, {}\n".format(item_id, man_name, item_type, price))
-
-                else:
-                    print("No such item in inventory")
+    inp=input("Enter Q to Exit")
+    if inp == "Q" or inp=="q":
+        break
